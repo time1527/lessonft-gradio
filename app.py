afda36dea85fda4405e6b4e398174f2f753a8a86
time1527 <154412155+time1527@users.noreply.github.com>
2024-04-17 17:25:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ os.system(f'git clone https://code.openxlab.org.cn/q4171119/lessonft.git {base_p
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
