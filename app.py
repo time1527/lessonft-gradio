b30ca32e4772366164b3c39169ed7299ad260a14
time1527 <154412155+time1527@users.noreply.github.com>
2024-04-17 13:45:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './lessonft'
-os.system(f'https://code.openxlab.org.cn/q4171119/lessonft.git {base_path}')
+base_path = './internlm2-chat-7b'
+os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
@@ -16,8 +16,32 @@ def chat(message,history):
         yield response
 
 gr.ChatInterface(chat,
-                 title="InternLM2-Chat-1.8B-Self-Cognition",
+                 title="InternLM2-Chat-7B",
                 description="""
 InternLM is mainly developed by Shanghai AI Laboratory.  
                  """,
                  ).queue(1).launch()
+
+# import gradio as gr
+# import os
+# import torch
+# from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+
+# # download internlm2 to the base_path directory using git tool
+# base_path = './lessonft'
+# os.system(f'https://code.openxlab.org.cn/q4171119/lessonft.git {base_path}')
+# os.system(f'cd {base_path} && git lfs pull')
+
+# tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+# model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+
+# def chat(message,history):
+#     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+#         yield response
+
+# gr.ChatInterface(chat,
+#                  title="InternLM2-Chat-1.8B-Self-Cognition",
+#                 description="""
+# InternLM is mainly developed by Shanghai AI Laboratory.  
+#                  """,
+#                  ).queue(1).launch()
