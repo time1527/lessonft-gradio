0f400e904ab182dcc333d5361538ff24978fada9
time1527 <154412155+time1527@users.noreply.github.com>
2024-04-17 13:24:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './internlm2-chat-1_8b_ft'
+base_path = './lessonft'
 os.system(f'https://code.openxlab.org.cn/q4171119/lessonft.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
